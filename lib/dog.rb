acc5549173deed0163de0bc34e898c0f73302dea
Nathan Burns <nathanandreburns@gmail.com>
2020-08-07 19:12:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog 
-  def name= (dogs_name)
-    @this_dogs_name = dogs_name
-  end 
-  
   def bark
   puts "Woof!"
 end
+
+  def name= (dogs_name)
+    @this_dogs_name = dogs_name
+  end 
 
 fido = Dog.new
 fido.bark 
