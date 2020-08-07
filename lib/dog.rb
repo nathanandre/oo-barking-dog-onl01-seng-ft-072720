a74c8d4db1494f06bb05d2e2007460e663ff95b9
Nathan Burns <nathanandreburns@gmail.com>
2020-08-07 19:14:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog 
+  def bark
+  puts "woof!"
+end
+
+fido = Dog.new
+fido.bark 
+
+  def name= (dogs_name)
+    @this_dogs_name = dogs_name
+  end 
+  
+  def name
+    @this_dogs_name
+  end 
+end 
+
